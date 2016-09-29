3b374bcf6838eac8e455f16755bf768038b6b276
Sergey Panfilov <se-panfilov@ya.ru>
2016-09-29 22:34:28 +0200

    Refactoring and fixes

--- src/countries/russia.js
+++ src/countries/russia.js
@@ -1,56 +1,55 @@
 // eslint-disable-next-line no-undef
 COUNTRIES.russia = (() => {
 
-    // const regex = [/^(RU)(\d{10}|\d{12})$/];
-    const regex = [/^(\d{10}|\d{12})$/];
-    const globalControlNumber = 11;
-    const validLength = {
-        short: 10,
-        long: 12
-    };
-    const controlSequence = {
-        short: [2, 4, 10, 3, 5, 9, 4, 6, 8],
-        long: [
-            [7, 2, 4, 10, 3, 5, 9, 4, 6, 8],
-            [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
-        ]
+    const rules = {
+        globalControlNumber: 11,
+        regex: [/^(\d{10}|\d{12})$/],
+        variations: {
+            short: {
+                _length: 10,
+                sequences: [
+                    [2, 4, 10, 3, 5, 9, 4, 6, 8]
+                ],
+                ctrlNumPos: [-1]
+            },
+            long: {
+                _length: 12,
+                sequences: [
+                    [7, 2, 4, 10, 3, 5, 9, 4, 6, 8],
+                    [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+                ],
+                ctrlNumPos: [-2, -1]
+            }
+        }
     };
 
-    const controlNumPosition = {
-        short: -1,
-        long: [-2, -1]
-    };
-
-    function checkVAT (vat, controlSequence, controlNumPosition) {
-        const valArr = vat.toString().split('').map(v => +v);
+    function checkSum (num, controlSequence, controlNumPosition) {
+        const valArr = num.toString().split('').map(v => +v);
         const controlNum = valArr[controlNumPosition];
         const numberArr = valArr.slice(0, controlNumPosition);
         const sum = numberArr.map((v, k) => v * controlSequence[k]).reduce((p, c) => p + c);
 
-        return (sum % globalControlNumber) === controlNum;
+        return (sum % rules.globalControlNumber) === controlNum;
     }
 
     function check10DigitVAT (vat) {
         // vat = 7830002293;
-        return checkVAT(vat, controlSequence.short, vat.toString().length + controlNumPosition.short);
+        const short = rules.variations.short;
+        return checkSum(vat, short.sequences[0], short._length + short.ctrlNumPos[0]);
     }
 
     function check12DigitVAT (vat) {
         //val = 500100732259;
-        const isFirstNumValid = checkVAT(vat, controlSequence.long[0], validLength.short + controlNumPosition.long[0]);
-        const isSecondNumValid = checkVAT(vat, controlSequence.long[1], validLength.long + controlNumPosition.long[1]);
+        const long = rules.variations.long;
+        const isFirstNumValid = checkSum(vat, long.sequences[0], long._length + long.ctrlNumPos[0]);
+        const isSecondNumValid = checkSum(vat, long.sequences[1], long._length + long.ctrlNumPos[1]);
 
         return isFirstNumValid && isSecondNumValid;
     }
 
     return {
-        getValidLength () {
-            return validLength;
-        },
-        getRegexp () {
-            return regex;
-        },
-        checkSum (vat) {
+        rules: rules,
+        checkVat (vat, options) {
             return check10DigitVAT(vat) || check12DigitVAT(vat);
         }
     };
--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 const COUNTRIES = {};
 
+function checkLength (length, rules) {
+    const variations = Object.keys(rules.variations);
+    const arr = [];
+    variations.forEach(v => arr.push(rules.variations[v]._length));
+
+    return arr.some(v => v === length);
+}
+
 // eslint-disable-next-line no-unused-vars
 var toExport = {
     checkINN: (value, country = 'russia') => {
-        const lengthList = COUNTRIES[country].getValidLength();
-        if (!lengthList.includes(value.length)) return 'invalid length';
+        const rules = COUNTRIES[country].rules;
+        if (!checkLength(value.length, rules)) return 'invalid length';
 
-        const regexp = COUNTRIES[country].getRegexp();
-        if (!regexp.test(value)) return 'don\'t match regexp';
+        if (!rules.regex.test(value)) return 'don\'t match regex';
 
-        if (!COUNTRIES[country].checkSum(value)) return 'checksum didn\'t match';
+        if (!COUNTRIES[country].checkVat(value)) return 'checksum didn\'t match';
 
         return true;
     }
