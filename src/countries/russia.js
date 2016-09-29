f6ed28045cf469e5faebe7c6e47a4722b8ff849e
Sergey Panfilov <se-panfilov@ya.ru>
2016-09-29 20:30:48 +0200

    Refactoring for calculations

--- src/countries/russia.js
+++ src/countries/russia.js
@@ -1,46 +1,52 @@
+// eslint-disable-next-line no-undef
 COUNTRIES.russia = (() => {
 
-    const regex = [/^(RU)(\d{10}|\d{12})$/];
+    // const regex = [/^(RU)(\d{10}|\d{12})$/];
+    const regex = [/^(\d{10}|\d{12})$/];
+    const globalControlNumber = 11;
+    const validLength = {
+        short: 10,
+        long: 12
+    };
+    const controlSequence = {
+        short: [2, 4, 10, 3, 5, 9, 4, 6, 8],
+        long: [
+            [7, 2, 4, 10, 3, 5, 9, 4, 6, 8],
+            [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8]
+        ]
+    };
 
-    function _check10DigitINN (val) {
-        // val = 7830002293;
-        const valArr = val.toString().split('');
-        const multipliers = [2, 4, 10, 3, 5, 9, 4, 6, 8];
-        const controlNum = valArr[valArr.length - 1];
-        const numberArr = valArr.slice(0, valArr.length - 1);
-        const sum = numberArr.map((v, k) => +v * +multipliers[k]).reduce((p, c) => p + c);
+    function checkVAT (vat, controlSequence, controlNumPosition) {
+        const valArr = vat.toString().split('').map(v => +v);
+        const controlNum = valArr[controlNumPosition];
+        const numberArr = valArr.slice(0, controlNumPosition);
+        const sum = numberArr.map((v, k) => v * controlSequence[k]).reduce((p, c) => p + c);
 
-        return (sum % 11) === controlNum;
+        return (sum % globalControlNumber) === controlNum;
     }
 
-    function _check12DigitINN (val) {
+    function check10DigitVAT (vat) {
+        // vat = 7830002293;
+        return checkVAT(vat, controlSequence.short, vat.toString().length - 1);
+    }
+
+    function check12DigitVAT (vat) {
         //val = 500100732259;
-        const valArr = val.toString().split('').map(v => +v);
+        const isFirstNumValid = checkVAT(vat, controlSequence.long[0], vat.toString().length - 2);
+        const isSecondNumValid = checkVAT(vat, controlSequence.long[1], vat.toString().length - 1);
 
-        const multipliers1 = [7, 2, 4, 10, 3, 5, 9, 4, 6, 8];
-        const multipliers2 = [3, 7, 2, 4, 10, 3, 5, 9, 4, 6, 8];
-
-        const controlNum1 = valArr[valArr.length - 2];
-        const controlNum2 = valArr[valArr.length - 1];
-
-        const numberArr1 = valArr.slice(0, valArr.length - 2);
-        const numberArr2 = valArr.slice(0, valArr.length - 1);
-
-        const sum1 = numberArr1.map((v, k) => v * +multipliers1[k]).reduce((p, c) => p + c);
-        const sum2 = numberArr2.map((v, k) => v * +multipliers2[k]).reduce((p, c) => p + c);
-
-        return (sum1 % 11) === controlNum1 && (sum2 % 11) === controlNum2;
+        return isFirstNumValid && isSecondNumValid;
     }
 
     return {
         getValidLength () {
-            return [10, 12];
+            return validLength;
         },
         getRegexp () {
             return regex;
         },
-        checkSum (val) {
-            return _check10DigitINN(val, this.rules) || _check12DigitINN(val, this.rules);
+        checkSum (vat) {
+            return check10DigitVAT(vat) || check12DigitVAT(vat);
         }
     };
 })();
